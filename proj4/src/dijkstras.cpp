88d8c06f3cea248a7fcf46a6e4c79d330276e127
Nicholas Andrew Wandinata <wrx481@hydra0.eecs.utk.edu>
2023-03-24 16:02:11 -0400

    Dijkstras Set Up

--- proj4/src/dijkstras.cpp
+++ proj4/src/dijkstras.cpp
@@ -1,4 +1,4 @@
-// dijsktras.cpp
+// dijkstras.cpp
 #include <iostream>
 #include <map>
 #include <vector>
@@ -10,6 +10,9 @@ int main(int argc, char *argv[]) {
 	char tileType;
 	map<char, int> costs;
 	vector<int> mapCosts; // Regular vector representing 2D vector
+	vector<int> backLinks; // Stores index of nodes (use -1 instead of NULL)
+	vector<int> distances; // Cost to go from start to node (-1 if unvisited)
+
 
 	while(cin >> nTiles) {
 		// Stores info of each tile type
@@ -18,20 +21,20 @@ int main(int argc, char *argv[]) {
 			costs.insert({tileType, tileCost});
 		}
 
-		// Reads in the map, stores as cost, and set up Dijsktras
+		// Reads in the map, stores as cost, and set up Dijkstras
 		cin >> nRows >> nCols;
 		for(int i = 0; i < nRows * nCols; i++) {
 			cin >> tileType;
 			tileCost = costs[tileType];
 			mapCosts.push_back(tileCost);
-
-
+			backLinks.push_back(-1);
+			distances.push_back(-1);
 		}
 		
 		// Read in runner's path info
 		cin >> sRow >> sCol >> eRow >> eCol;
 
-		// Run Dijsktras
+		// Run Dijkstras
 		// Need adjacency list
 	}
 	return 0;
