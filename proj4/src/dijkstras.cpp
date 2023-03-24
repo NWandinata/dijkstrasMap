4ac85263cc8fe5454ebae90b2a3f63cc4ba19fbd
Nicholas Andrew Wandinata <wrx481@hydra0.eecs.utk.edu>
2023-03-24 09:20:01 -0400

    Read input

--- proj4/src/dijkstras.cpp
+++ proj4/src/dijkstras.cpp
@@ -1,8 +1,30 @@
 // dijsktras.cpp
+#include <iostream>
+#include <map>
 
-// Main Execution
+using namespace std;
 
 int main(int argc, char *argv[]) {
-    return 0;
+	int nTiles, tileCost, nRows, nCols, sRow, sCol, eRow, eCol;
+	char tileType;
+	map<char, int> costs;
+
+	while(cin >> nTiles) {
+		// Stores info of each tile type
+		for(int i = 0; i < nTiles; i++) {
+			cin >> tileType >> tileCost;
+			costs.insert({tileType, tileCost});
+		}
+
+		cin >> nRows >> nCols;
+		// Read in tiles and store into container
+		// Regular vector representing 2D vector?
+		
+		// Read in runner's path info
+		cin >> sRow >> sCol >> eRow >> eCol;
+
+		// Run Dijsktras
+	}
+	return 0;
 }
 
